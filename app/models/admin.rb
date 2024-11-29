505462dcea40162ce29b57aba837a1c3231cac70
Alfeus Bintang Johandy Yahya <johandyyahya737@gmail.com>
2024-11-29 15:40:13 +0700

    feat: Add SignUp Admin Dashboards

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,14 @@ class Admin < ApplicationRecord
 
     has_secure_password
 
-    enum :roles, { property: 0, tender: 1, penyewaan: 2, package: 3, kpr: 4 }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+    enum :roles, {
+        property: 0, 
+        tender: 1, 
+        penyewaan: 2, 
+        package: 3, 
+        kpr: 4, 
+        super_admin: 5
+    }
 end
