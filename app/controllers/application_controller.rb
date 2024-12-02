b3c9f2cb38e2a9175663e6c72d5d63c33ebaf05b
iankusmon <muh.rahardian.k@gmail.com>
2024-12-02 13:28:46 +0700

    chore: undo changes in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::API
-  include ActionController::RequestForgeryProtection
-  protect_from_forgery with: :exception
+  # include ActionController::RequestForgeryProtection
+  # protect_from_forgery with: :exception
 
-  before_action :verify_authenticity_token, unless: -> { request.format.json? }
+  # before_action :verify_authenticity_token, unless: -> { request.format.json? }
 
-  attr_reader :current_admin
+  # attr_reader :current_admin
 
-  # private
+  # # private
 
-  def authentication_admin
-    token = request.headers['Authorization']&.split(' ')&.last
-    @current_admin = Admin.find_by(auth_token: token)
+  # def authentication_admin
+  #   token = request.headers['Authorization']&.split(' ')&.last
+  #   @current_admin = Admin.find_by(auth_token: token)
 
-    render json: { message: 'Unauthorized access' }, status: :unauthorized unless @current_admin
-  end
+  #   render json: { message: 'Unauthorized access' }, status: :unauthorized unless @current_admin
+  # end
 end
 
