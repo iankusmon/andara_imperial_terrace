5c799265eb22083142e5fc037a6e0689fa0eafbe
iankusmon <muh.rahardian.k@gmail.com>
2025-02-13 01:38:05 +0700

    fix indent

--- app/controllers/api/customers/ajb_documents_controller.rb
+++ app/controllers/api/customers/ajb_documents_controller.rb
@@ -1,41 +1,40 @@
 module Api
   module Customers
-  class AjbDocumentsController < ApplicationController
+    class AjbDocumentsController < ApplicationController
       # Private Endpoint
-      
+
       # POST /ajbs
       def create
-          ajb_params = create_request
+        ajb_params = create_request
 
-          if Ajb.find_by(ajb_number: ajb_params[:ajb_number])
+        if Ajb.find_by(ajb_number: ajb_params[:ajb_number])
           render json: { error: "This Ajb already exists" }, status: :bad_request
-          else
+        else
           ajb = Ajb.create!(ajb_params)
           render json: ajb, status: :ok
-          end
+        end
       end
 
       private
 
       def create_request
-          params.require(:ajb).permit(
-                :ajb_number,
-                :booking_fee_number,
-                :down_payment_number,
-                :order_number,
-                :payment_type,
-                :payment_method,
-                :cash_tempo_period,
-                :fullname,
-                :nik,
-                :occupation,
-                :status,
-                :documentation_photo,
-                :scan_ppjb_document,
-                :file
-          )
+        params.require(:ajb).permit(
+          :ajb_number,
+          :booking_fee_number,
+          :down_payment_number,
+          :order_number,
+          :payment_type,
+          :payment_method,
+          :cash_tempo_period,
+          :fullname,
+          :nik,
+          :occupation,
+          :status,
+          :documentation_photo,
+          :scan_ppjb_document,
+          :file
+        )
       end
-      
-    end        
+    end
   end
 end
