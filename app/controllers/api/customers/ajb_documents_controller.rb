347f8ce9b697d68363f3c83378b81269e21e5191
iankusmon <muh.rahardian.k@gmail.com>
2025-02-13 01:43:05 +0700

    temp: commented ajb controller

--- app/controllers/api/customers/ajb_documents_controller.rb
+++ app/controllers/api/customers/ajb_documents_controller.rb
@@ -1,40 +1,41 @@
-module Api
-  module Customers
-    class AjbDocumentsController < ApplicationController
-      # Private Endpoint
+# module Api
+#   module Customers
+#     class AjbDocumentsController < ApplicationController
+#       # Private Endpoint
 
-      # POST /ajbs
-      def create
-        ajb_params = create_request
+#       # POST /ajbs
+#       def create
+#         ajb_params = create_request
 
-        if Ajb.find_by(ajb_number: ajb_params[:ajb_number])
-          render json: { error: "This Ajb already exists" }, status: :bad_request
-        else
-          ajb = Ajb.create!(ajb_params)
-          render json: ajb, status: :ok
-        end
-      end
+#         if Ajb.find_by(ajb_number: ajb_params[:ajb_number])
+#           render json: { error: "This Ajb already exists" }, status: :bad_request
+#         else
+#           ajb = Ajb.create!(ajb_params)
+#           render json: ajb, status: :ok
+#         end
+#       end
 
-      private
+#       private
 
-      def create_request
-        params.require(:ajb).permit(
-          :ajb_number,
-          :booking_fee_number,
-          :down_payment_number,
-          :order_number,
-          :payment_type,
-          :payment_method,
-          :cash_tempo_period,
-          :fullname,
-          :nik,
-          :occupation,
-          :status,
-          :documentation_photo,
-          :scan_ppjb_document,
-          :file
-        )
-      end
-    end
-  end
-end
+#       def create_request
+#         params.require(:ajb).permit(
+#           :ajb_number,
+#           :booking_fee_number,
+#           :down_payment_number,
+#           :order_number,
+#           :payment_type,
+#           :payment_method,
+#           :cash_tempo_period,
+#           :fullname,
+#           :nik,
+#           :occupation,
+#           :status,
+#           :documentation_photo,
+#           :scan_ppjb_document,
+#           :file
+#         )
+#       end
+
+#     end
+#   end
+# end
