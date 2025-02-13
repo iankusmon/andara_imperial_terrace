3c6b2d61c6f3f886d01cf6f9007938049b23a32d
iankusmon <muh.rahardian.k@gmail.com>
2025-02-13 15:52:07 +0700

    fix: reset not exist refence

--- db/migrate/20250212202215_create_agent_affiliate_commissions.rb
+++ db/migrate/20250212202215_create_agent_affiliate_commissions.rb
@@ -2,7 +2,7 @@ class CreateAgentAffiliateCommissions < ActiveRecord::Migration[7.0]
   def change
     create_table :agent_affiliate_commissions do |t|
       t.references :agent_affiliate, null: false, foreign_key: true
-      t.references :property_unit, null: false, foreign_key: true
+      # t.references :property_unit, null: false, foreign_key: true
       t.decimal :unit_price, precision: 15, scale: 2, null: false
       t.decimal :commission_percentage, precision: 5, scale: 2, default: 2.0
       t.decimal :commission_amount, precision: 15, scale: 2, null: false
