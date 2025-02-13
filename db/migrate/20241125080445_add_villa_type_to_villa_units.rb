8d6f0bc6aa73eb244c7918cfebd3050f22b29006
iankusmon <muh.rahardian.k@gmail.com>
2025-02-13 15:03:57 +0700

    fix: AddVillaTypeToVillaUnits migration file

--- db/migrate/20241125080445_add_villa_type_to_villa_units.rb
+++ db/migrate/20241125080445_add_villa_type_to_villa_units.rb
@@ -1,5 +1,5 @@
 class AddVillaTypeToVillaUnits < ActiveRecord::Migration[8.0]
   def change
-    add_column :villa_units, :villa_type, :integer
+    # add_column :villa_units, :villa_type, :integer
   end
 end
